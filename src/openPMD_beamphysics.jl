b18a03566a83361dbdcdb38a6b95873b0b9a8a54
ndwang <ndwang3@gmail.com>
2025-05-30 03:50:39 -0400

    export newly added functions

--- src/openPMD_beamphysics.jl
+++ src/openPMD_beamphysics.jl
@@ -16,6 +16,8 @@ include("units.jl")
 include("readers.jl")
 include("statistics.jl")
 include("particles.jl")
+include("labels.jl")
+include("plot.jl")
 
 # Export key types and functions
 export particle_paths, field_paths, is_constant_component, constant_component_value
@@ -26,6 +28,8 @@ export component_data, is_constant_component, constant_component_value
 export norm_emit_calc, twiss_calc, twiss_dispersion
 export ParticleGroup, single_particle, twiss, in_z_coordinates, in_t_coordinates, average_current
 export drift!, drift_to_t!, drift_to_z!, split_particles
+export limits, plottable_array
+export mathlabel, texlabel
 
 
 # Version info
